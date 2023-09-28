b657e56ea13c1895efc35808b53b00849e4518c3
me <me@zoomido.se>
2023-09-28 23:15:42 +0200

    Use lazy loading example from https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/lazy-loading-with-lazy-nvim.md

--- nvim/nvim-lua/lua/init.lua
+++ nvim/nvim-lua/lua/init.lua
@@ -69,27 +69,88 @@ require('lazy').setup({
         },
     },
 
+    -- LSP from Zero to Hero
     {
-        -- LSP from zero to hero
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         lazy = true,
-        config = false,
+        init = function()
+            -- Disable automatic setup, we are doing it manually
+            vim.g.lsp_zero_extend_cmp = 0
+            vim.g.lsp_zero_extend_lspconfig = 0
+        end,
         dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'}, -- Required
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-            -- Additional lua configuration, makes nvim stuff amazing!
-            'folke/neodev.nvim',
-        },
+            {'folke/neodev.nvim', opts = {}}
+        }
+    },
+    {
+        'williamboman/mason.nvim',
+        lazy = false,
+        config = true,
     },
 
-    -- Manage LSP servers from nvim
-    {'williamboman/mason.nvim', event = 'VeryLazy'},
-    {'williamboman/mason-lspconfig.nvim', event = 'VeryLazy'},
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'},
+        },
+        config = function()
+            -- Here is where you configure the autocompletion settings.
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_cmp()
+
+            -- And you can configure cmp even more, if you want to.
+            local cmp = require('cmp')
+            local cmp_action = lsp_zero.cmp_action()
+
+            cmp.setup({
+                formatting = lsp_zero.cmp_format(),
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                })
+            })
+        end
+    },
+
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+        event = {'BufReadPre', 'BufNewFile'},
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'williamboman/mason-lspconfig.nvim'},
+        },
+        config = function()
+            -- This is where all the LSP shenanigans will live
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
+
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
+
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = {
+                    lsp_zero.default_setup,
+                    lua_ls = function()
+                        -- (Optional) Configure lua language server for neovim
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        require('lspconfig').lua_ls.setup(lua_opts)
+                    end,
+                }
+            })
+        end
+    },
 
     {
         -- Adds git related signs to the gutter, as well as utilities for managing changes
@@ -285,23 +346,23 @@ require('lazy').setup({
 --
 -- LSP Zero setup
 --
-local lsp_zero = require('lsp-zero')
-lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
-end)
+-- local lsp_zero = require('lsp-zero')
+-- lsp_zero.on_attach(function(client, bufnr)
+--     -- see :help lsp-zero-keybindings
+--     -- to learn the available actions
+--     lsp_zero.default_keymaps({buffer = bufnr})
+-- end)
 
 -- Setup default options for lua_ls. Used for fixing issue with neovim config
-require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
+-- require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
 
 -- Setup Mason with lspconfig integration
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    handlers = {
-        lsp_zero.default_setup,
-    },
-})
+-- require('mason').setup({})
+-- require('mason-lspconfig').setup({
+--     handlers = {
+--         lsp_zero.default_setup,
+--     },
+-- })
 
 -- old copy
 -- local lsp = require('lsp-zero').preset({})
