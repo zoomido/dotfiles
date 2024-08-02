e389378c7d2155bbba6690091450aacbd17b1b4d
zoomido <me@zoomido.se>
2024-08-02 17:02:58 +0200

    Format with lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -29,12 +29,12 @@ mash = { 'shift', 'ctrl', 'cmd' }
 hs.hotkey.bind({}, '´', function()
     hs.eventtap.keyStrokes("`")
 end)
-hs.hotkey.bind({"shift"}, '´', function()
+hs.hotkey.bind({ "shift" }, '´', function()
     hs.eventtap.keyStrokes("´")
 end)
 
 -- Send tilde directly
-hs.hotkey.bind({'alt'}, '¨', function()
+hs.hotkey.bind({ 'alt' }, '¨', function()
     hs.eventtap.keyStrokes("~")
 end)
 
@@ -47,7 +47,7 @@ end)
 -- Start/switch/hide Kitty
 hs.hotkey.bind({}, "§", function()
     local app = hs.application.find('kitty')
-    if ( app and app:isFrontmost() ) then
+    if (app and app:isFrontmost()) then
         app:hide()
     else
         hs.application.open("kitty.app")
@@ -58,30 +58,30 @@ hs.hotkey.bind({}, "§", function()
 end)
 
 -- Start/switch/hide browser
-hs.hotkey.bind({"cmd"}, "§", function()
+hs.hotkey.bind({ "cmd" }, "§", function()
     local app = hs.application.find('brave')
-    if ( app and app:isFrontmost() ) then
+    if (app and app:isFrontmost()) then
         app:hide()
     else
         hs.application.open("Brave Browser.app")
     end
 end)
 -- Start/switch to browser and reload
-hs.hotkey.bind({"cmd", "shift"}, "return", function()
+hs.hotkey.bind({ "cmd", "shift" }, "return", function()
     -- hs.eventtap.keyStrokes(":w")
     -- hs.eventtap.keyStroke({}, "return")
     hs.application.open("Brave Browser.app")
-    hs.eventtap.keyStroke({"cmd"}, "2")
+    hs.eventtap.keyStroke({ "cmd" }, "2")
     hs.timer.doAfter(0.1, function()
-        hs.eventtap.keyStroke({"cmd"}, "r")
+        hs.eventtap.keyStroke({ "cmd" }, "r")
     end)
 end)
 
 
 -- Start/switch/hide note application
-hs.hotkey.bind({"shift"}, "§", function()
+hs.hotkey.bind({ "shift" }, "§", function()
     local app = hs.application.find('Alacritty')
-    if ( app and app:isFrontmost() ) then
+    if (app and app:isFrontmost()) then
         app:hide()
     else
         hs.application.open("Alacritty.app")
@@ -89,10 +89,21 @@ hs.hotkey.bind({"shift"}, "§", function()
 end)
 
 
--- Start/switch/hide Spotify
+-- Start/switch/hide Messaging app
 hs.hotkey.bind(mash, "m", function()
+    local app = hs.application.find('Rambox')
+    if (app and app:isFrontmost()) then
+        app:hide()
+    else
+        hs.application.open("Rambox.app")
+    end
+end)
+
+
+-- Start/switch/hide Spotify
+hs.hotkey.bind(mash, "s", function()
     local app = hs.application.find('spotify')
-    if ( app and app:isFrontmost() ) then
+    if (app and app:isFrontmost()) then
         app:hide()
     else
         hs.application.open("Spotify.app")
@@ -103,46 +114,46 @@ end)
 -- For center screen and make it almost fullscreen
 hs.window.animationDuration = 0
 Units = {
-  fullscreen        = { x = 0.0, y = 0.0, w = 1, h = 1 },
-  almostFullscreen  = { x = 0.025, y = 0.04, w = 0.946, h = 0.930 },
-  centerSmall       = { x = 0.1, y = 0.1, w = 0.8, h = 0.8 },
-  leftHalf       = { x = 0, y = 0, w = 0.495, h = 1 },
-  rightHalf       = { x = 0.501, y = 0, w = 0.5, h = 1 },
-  topHalf       = { x = 0, y = 0, w = 1, h = 0.495 },
-  bottomHalf       = { x = 0, y = 0.501, w = 1, h = 0.5 },
-  smallMobile       = { x = 0.02, y = 0.02, w = 0.346, h = 0.975 },
-  inspectorChrome   = { x = 0.366, y = 0.02, w = 0.61, h = 0.975 }
+    fullscreen       = { x = 0.0, y = 0.0, w = 1, h = 1 },
+    almostFullscreen = { x = 0.025, y = 0.04, w = 0.946, h = 0.930 },
+    centerSmall      = { x = 0.1, y = 0.1, w = 0.8, h = 0.8 },
+    leftHalf         = { x = 0, y = 0, w = 0.495, h = 1 },
+    rightHalf        = { x = 0.501, y = 0, w = 0.5, h = 1 },
+    topHalf          = { x = 0, y = 0, w = 1, h = 0.495 },
+    bottomHalf       = { x = 0, y = 0.501, w = 1, h = 0.5 },
+    smallMobile      = { x = 0.02, y = 0.02, w = 0.346, h = 0.975 },
+    inspectorChrome  = { x = 0.366, y = 0.02, w = 0.61, h = 0.975 }
 }
 hs.hotkey.bind(mash, 'f', function()
-        hs.window.focusedWindow():move(Units.fullscreen,  nil, true)
+    hs.window.focusedWindow():move(Units.fullscreen, nil, true)
 end)
 hs.hotkey.bind(mash, 'a', function()
-        hs.window.focusedWindow():move(Units.almostFullscreen,  nil, true)
+    hs.window.focusedWindow():move(Units.almostFullscreen, nil, true)
 end)
 hs.hotkey.bind(mash, 'c', function()
-        hs.window.focusedWindow():move(Units.centerSmall,  nil, true)
+    hs.window.focusedWindow():move(Units.centerSmall, nil, true)
 end)
 hs.hotkey.bind(mash, 'left', function()
-        hs.window.focusedWindow():move(Units.leftHalf,  nil, true)
+    hs.window.focusedWindow():move(Units.leftHalf, nil, true)
 end)
 hs.hotkey.bind(mash, 'right', function()
-        hs.window.focusedWindow():move(Units.rightHalf,  nil, true)
+    hs.window.focusedWindow():move(Units.rightHalf, nil, true)
 end)
 hs.hotkey.bind(mash, 'up', function()
-        hs.window.focusedWindow():move(Units.topHalf,  nil, true)
+    hs.window.focusedWindow():move(Units.topHalf, nil, true)
 end)
 hs.hotkey.bind(mash, 'down', function()
-        hs.window.focusedWindow():move(Units.bottomHalf,  nil, true)
+    hs.window.focusedWindow():move(Units.bottomHalf, nil, true)
 end)
 hs.hotkey.bind(mash, 's', function()
-    hs.window.focusedWindow():move(Units.smallMobile,  nil, true)
+    hs.window.focusedWindow():move(Units.smallMobile, nil, true)
 end)
 hs.hotkey.bind(mash, 'i', function()
-    hs.window.focusedWindow():move(Units.inspectorChrome,  nil, true)
+    hs.window.focusedWindow():move(Units.inspectorChrome, nil, true)
 end)
 -- Send mouse away
 hs.hotkey.bind(mash, "ö", function()
-    hs.mouse.setRelativePosition({ x=1400, y=90 })
+    hs.mouse.setRelativePosition({ x = 1400, y = 90 })
 end)
 
 
@@ -153,9 +164,9 @@ end)
 -- set up your windowfilter
 -- switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
 -- switcher_browsers = hs.window.switcher.new{'Safari','Google Chrome'} -- specialized switcher for your dozens of browser windows :)
-switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
+switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter {}) -- include minimized/hidden windows, current Space only
 switcher.ui.showTitles = false
-switcher.ui.backgroundColor = {0.1,0.1,0.1,0.3}
+switcher.ui.backgroundColor = { 0.1, 0.1, 0.1, 0.3 }
 switcher.ui.selectedThumbnailSize = 300
 switcher.ui.showSelectedTitle = false
 switcher.ui.fontName = 'Helvetica'
@@ -168,15 +179,16 @@ switcher.ui.fontName = 'Helvetica'
 function mapCmdTab(event)
     local flags = event:getFlags()
     local chars = event:getCharacters()
-    if chars == "\t" and flags:containExactly{'cmd'} then
+    if chars == "\t" and flags:containExactly { 'cmd' } then
         switcher:next()
         return true
-    elseif chars == string.char(25) and flags:containExactly{'cmd','shift'} then
+    elseif chars == string.char(25) and flags:containExactly { 'cmd', 'shift' } then
         switcher:previous()
         return true
     end
 end
-tapCmdTab = hs.eventtap.new({hs.eventtap.event.types.keyDown}, mapCmdTab)
+
+tapCmdTab = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, mapCmdTab)
 tapCmdTab:start()
 
 
@@ -192,7 +204,7 @@ hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://gith
 ---------------------
 hs.loadSpoon("GlobalMute")
 spoon.GlobalMute:bindHotkeys({
-    toggle = {{"ctrl","cmd","shift"}, "z"}
+    toggle = { { "ctrl", "cmd", "shift" }, "z" }
 })
 spoon.GlobalMute:configure({
     enforce_desired_state = true,
@@ -228,6 +240,7 @@ function muteWatcher(uid, event)
     end
     lastMuteValue = inputDevice:muted()
 end
+
 inputDevice:watcherCallback(muteWatcher)
 inputDevice:watcherStart()
 
@@ -273,7 +286,7 @@ batteryWatcher = hs.battery.watcher.new(function()
         if currentBatValue > 71 and currentBatValue > lastBatValue then
             batteryInfoWindow = hs.alert.show('Battery charged up! (' .. math.floor(tonumber(currentBatValue)) .. '%)', {
                 strokeColor = hs.drawing.color.x11.cadetblue,
-                fillColor = {["red"]=0.100,["green"]=0.150,["blue"]=0.100,["alpha"]=0.6},
+                fillColor = { ["red"] = 0.100, ["green"] = 0.150, ["blue"] = 0.100, ["alpha"] = 0.6 },
                 textColor = hs.drawing.color.x11.cadetblue,
                 strokeWidth = 10,
                 radius = 10,
@@ -292,7 +305,7 @@ batteryWatcher = hs.battery.watcher.new(function()
     if (currentBatValue == 50 or currentBatValue == 49 or currentBatValue == 40 or currentBatValue == 39) and currentBatValue < lastBatValue then
         batteryInfoWindow = hs.alert.show('🔋 ' .. math.floor(tonumber(currentBatValue)) .. '%', {
             strokeColor = hs.drawing.color.x11.silver,
-            fillColor = {["red"]=0.400,["green"]=0.350,["blue"]=0.350,["alpha"]=0.7},
+            fillColor = { ["red"] = 0.400, ["green"] = 0.350, ["blue"] = 0.350, ["alpha"] = 0.7 },
             textColor = hs.drawing.color.x11.silver,
             strokeWidth = 10,
             radius = 10,
@@ -307,7 +320,7 @@ batteryWatcher = hs.battery.watcher.new(function()
     if currentBatValue < 35 and currentBatValue < lastBatValue then
         batteryInfoWindow = hs.alert.show('🪫 Live or let die: ' .. math.floor(tonumber(currentBatValue)) .. '%', {
             strokeColor = hs.drawing.color.x11.goldenrod,
-            fillColor = {["red"]=0.400,["green"]=0.200,["blue"]=0.200,["alpha"]=0.7},
+            fillColor = { ["red"] = 0.400, ["green"] = 0.200, ["blue"] = 0.200, ["alpha"] = 0.7 },
             textColor = hs.drawing.color.x11.goldenrod,
             strokeWidth = 10,
             radius = 10,
@@ -317,7 +330,7 @@ batteryWatcher = hs.battery.watcher.new(function()
             atScreenEdge = 0
         }, 5)
         menuBatteryTitle = hs.styledtext.new(math.floor(currentBatValue) .. '%', {
-            color = { red = 1, green = 0.5, blue = 0.5},
+            color = { red = 1, green = 0.5, blue = 0.5 },
         })
     end
 
@@ -355,4 +368,3 @@ end
 notificationName = "AppleInterfaceThemeChangedNotification"
 appearanceWatcher = hs.distributednotifications.new(cb, notificationName, nil)
 appearanceWatcher:start()
-
