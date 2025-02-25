0c2f50d0af0c56d529e4ebcb9ee8398ac48006e3
zoomido <me@zoomido.se>
2025-02-25 14:09:42 +0100

    Remove deprecated plugin

--- nvim/nvim-lua/lua/init.lua
+++ nvim/nvim-lua/lua/init.lua
@@ -543,20 +543,6 @@ require('lazy').setup({
         },
     },
 
-    {
-        'dpayne/CodeGPT.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'MunifTanjim/nui.nvim',
-        },
-        config = function()
-            require('codegpt.config')
-        end,
-        opts = {
-            model = 'gpt-4-turbo',
-        },
-    },
-
     -- {
     --     'olimorris/codecompanion.nvim',
     --     dependencies = {
