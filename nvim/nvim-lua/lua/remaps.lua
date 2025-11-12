36c5b60319f6617b96ba61886975c3509474629c
zoomido <35059247+zoomido@users.noreply.github.com>
2025-11-12 17:58:28 +0100

    Update remaps.lua

--- nvim/nvim-lua/lua/remaps.lua
+++ nvim/nvim-lua/lua/remaps.lua
@@ -1,20 +1,31 @@
 -- remaps.lua
 
--- [[ Basic vim key remaps ]]
--- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 -- {mode} Mode short-name (map command prefix: "n", "i", "v", "x", …) or "!" for :map!, or empty string for :map. "ia", "ca" or "!a" for abbreviation in Insert mode, Cmdline mode, or both, respectively
 
+--
+-- General keybinds
+--
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true }) -- Holding down space wont repeat
+vim.keymap.set({ 'n' }, '<C-s>', '<Cmd>w<Cr>', { silent = true, desc = 'Save current buffer' })
+vim.keymap.set({ 'n', 't' }, '<C-q>', '<Cmd>q<Cr>', { silent = true, desc = 'Close current buffer' })
+vim.keymap.set({ 'n', 't' }, '<C-x>', '<Cmd>bd<Cr>', { silent = true, desc = 'Close current window' })
+vim.keymap.set({ 'n' }, '<Leader><Leader>', '<Cmd>b#<Cr>', { silent = true, desc = 'Jump to previously edited buffer' })
+-- vim.keymap.set({ 'i' }, 'ii', '<Esc>', { silent = true, desc = 'Escape insert mode' })
+vim.keymap.set('v', 'J', ":m '>+1<Cr>gv=gv", { silent = true, desc = 'Move visual selection down' })
+vim.keymap.set('v', 'K', ":m '<-2<Cr>gv=gv", { silent = true, desc = 'Move visual selection up' })
+vim.keymap.set('n', 'v_', "^vg_", { silent = true, desc = 'Select line without line ending char' })
+
+
+--
+-- Navigation keybinds
+--
 -- vim.keymap.set({ 'n', 'v' }, '{', '[{', { silent = true, desc = 'Jump to parent bracket' })
 vim.keymap.set({ 'n', 'v' }, '<S-Tab>', '[{', { silent = true, desc = 'Jump to parent bracket' })
 -- vim.keymap.set({ 'n', 'v' }, '}', ']}', { silent = true, desc = 'Jump to next outer bracket' })
 vim.keymap.set({ 'n', 'v' }, '<Tab>', '%', { silent = true, desc = 'Jump between matching pairs' })
 -- vim.keymap.set({ 'n' }, '<Tab>', '<Plug>(MatchitNormalForward)', { silent = true, desc = 'Jump between matching pairs' })
 -- vim.keymap.set({ 'v' }, '<Tab>', '<Plug>(MatchitVisualForward)', { silent = true, desc = 'Jump between matching pairs' })
-vim.keymap.set({ 'n' }, '<C-s>', '<Cmd>w<Cr>', { silent = true, desc = 'Save current buffer' })
-vim.keymap.set({ 'n' }, '<Leader><Leader>', '<Cmd>b#<Cr>', { silent = true, desc = 'Jump to previously edited buffer' })
--- vim.keymap.set({ 'i' }, 'ii', '<Esc>', { silent = true, desc = 'Escape insert mode' })
 
 -- Left as a reminder, could use gj and gk for faster navigaton maybe but better to learn original binds?
 -- vim.keymap.set({ 'n' }, 'gj', ']c', { silent = true, desc = 'Jump to start of next change' })
@@ -30,7 +41,9 @@ vim.keymap.set({ 'n' }, '<C-l>', '<C-w>l', { silent = true, desc = 'Jump to righ
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true})
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true})
 
+--
 -- Better yank/cut/paste
+--
 -- Use capital P, it will paste in insert mode without overriding yank register (")
 vim.keymap.set({ 'n', 'v' }, 'x', '"_x', { silent = true, desc = 'Dont put deleted text into yank register' }) -- Deletes go to register 1-9
 vim.keymap.set({ 'n', 'v' }, 'X', '"_X', { silent = true, desc = 'Dont put deleted text into yank register' }) -- Deletes go to register 1-9
@@ -44,18 +57,11 @@ vim.keymap.set({ 'v' }, '+', '"0ygvd"', { silent = true, desc = 'Cut to yank reg
  --         ["+"] = { "\"0p", "Paste latest yank" },
 vim.keymap.set({ 'v' }, ':p', "y:<C-r>\"<C-b>", { silent = true, desc = 'Paste visual selection in cmdline' })
 
+--
 -- Search and replace
--- " Replace word under cursor
--- nnoremap <leader>r :%s/\<<C-r><C-w>\>//c<Left><Left>
--- vnoremap <leader>r :s/\<<C-r><C-w>\>//c<Left><Left>
+--
 vim.keymap.set('v', ':re', "\"hy:%s/<C-r>h//c<Left><Left>", { silent = true, desc = 'Replace the visual selection elsewhere' })
 vim.keymap.set('v', ':ri', ":s/\\%V\\%V/<Left><Left><Left><Left>", { silent = true, desc = 'Replace inside visual selection' })
-
-vim.keymap.set('v', 'J', ":m '>+1<Cr>gv=gv", { silent = true, desc = 'Move visual selection down' })
-vim.keymap.set('v', 'K', ":m '<-2<Cr>gv=gv", { silent = true, desc = 'Move visual selection up' })
-
-vim.keymap.set('n', 'v_', "^vg_", { silent = true, desc = 'Select line without line ending char' })
-
 
 --
 -- Abbreviations
